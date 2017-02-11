69acb25de6ad7931b88d8a780af92b7c04d61a3b
Damian Ospara <damian89m@gmail.com>
2017-02-11 12:01:21 +0100

    Create event generator

---
+++ project1/core/EventGenerator.py
@@ -0,0 +1,48 @@
+import time
+import random
+import uuid
+
+available_event_types = ["WIN", "CLICK"]
+available_campaign_ids = ["111", "222", "333", "444", "555"]
+available_creative_ids = ["12" , "22", "32", "42", "52"]
+available_device_categories = ["m", "d", "t"]
+
+
+def event_type_gen():
+    return random.choice(available_event_types)
+
+def bid_id_gen():
+    return str(uuid.uuid4())
+
+def timestamp_gen():
+    return int(time.time())
+
+def campaign_id_gen():
+    return random.choice(available_campaign_ids)
+
+def creative_id_gen():
+    return random.choice(available_creative_ids)
+
+def device_category_gen():
+    return random.choice(available_device_categories)
+
+def device_id_gen():
+    return str(uuid.uuid4())
+
+def generate_event():
+        temp = {"event": event_type_gen(),
+            "ts": timestamp_gen(),
+            "bidId": bid_id_gen(),
+            "campaignId": campaign_id_gen(),
+            "creativeId": creative_id_gen(),
+            "bid": {"price": 3.183, "currency": "USD"},
+            "deviceId": device_id_gen(),
+            "device": {"category": device_category_gen()}}
+        return temp
+
+def event_content_gen(lines_no=10):
+    for x in xrange(lines_no):
+        yield generate_event()
+
+if __name__ == "__main__":
+    print str(generate_event())
--- project1/core/TempFileGenerator.py
+++ project1/core/TempFileGenerator.py
@@ -1,21 +1,47 @@
 import uuid
 import os
 import os.path as path
+from EventGenerator import event_content_gen
+
+def number_generator(n):
+    for i in xrange(n):
+        yield str(i)
+
+def uuid_generator(n):
+    for i in xrange(n):
+        yield str(uuid.uuid4())
+
+def simple_content_gen():
+    return "OK"
+
+def gen_json():
+    return "{}"
+
+def json_content_gen(lines_no=10):
+    for x in xrange(lines_no):
+        yield gen_json()
 
 class TempFileGenerator:
 
-    def __init__(self, dir_path ="/tmp/"):
+    def __init__(self,
+                 dir_path ="/tmp/",
+                 file_name_gen=number_generator,
+                 content_gen=simple_content_gen):
         assert not path.exists(dir_path), "Temporary folder should not exists"
         self.dir_path = dir_path
+        self.file_name_gen = file_name_gen
+        self.content_gen = content_gen
 
-    def generate(self, n):
+    def generate(self, n, lines_no=10):
         os.makedirs(self.dir_path)
-        for i in xrange(n):
-            self.__create_file(self.dir_path, i, i)
+        for i in self.file_name_gen(n):
+            self.__create_file(self.dir_path, i, self.content_gen, lines_no)
 
-    def __create_file(self, dir_path, file_name, content):
+    @staticmethod
+    def __create_file(dir_path, file_name, content_gen, lines_no=10):
         with open(path.join(dir_path, str(file_name)), mode='w') as file:
-            file.write("%s" % str(content))
+            for line in content_gen(lines_no):
+                file.write("%s\n" % line)
 
 if __name__ == "__main__":
     print "Temporary file generator script"
@@ -23,10 +49,13 @@ if __name__ == "__main__":
     ''':type tempFolder: path'''
     tempFolder = path.join("/tmp/python", str(uuid.uuid4()))
 
-    ''':type generator: TempFileGenerator'''
-    generator = TempFileGenerator(tempFolder)
-    generator.generate(10)
+    files_no = 10
+    lines_no = 100000
 
-    assert path.exists(tempFolder), "Folder should be created by temp file generator"
+    ''':type generator: TempFileGenerator'''
+    generator = TempFileGenerator(tempFolder, uuid_generator, event_content_gen)
+    generator.generate(files_no, lines_no)
+
+    assert path.exists(tempFolder), "Folder should be created by temp file generator!"
     list = os.listdir(tempFolder)
-    print list
+    assert len(list) == files_no, "Generator did not create expected files number!"
