0fecf875c853915c0268da6a3907b16815bba29c
Damian Ospara <damian89m@gmail.com>
2017-02-19 13:34:28 +0100

    Add concurrency. Add script argparse

--- project1/core/TempFileGenerator.py
+++ project1/core/TempFileGenerator.py
@@ -1,7 +1,9 @@
 import uuid
 import os
 import os.path as path
+import threading
 from EventGenerator import event_content_gen
+import argparse
 
 def number_generator(n):
     for i in xrange(n):
@@ -26,16 +28,27 @@ class TempFileGenerator:
     def __init__(self,
                  dir_path ="/tmp/",
                  file_name_gen=number_generator,
-                 content_gen=simple_content_gen):
+                 content_gen=simple_content_gen,
+                 concurrent=True):
         assert not path.exists(dir_path), "Temporary folder should not exists"
         self.dir_path = dir_path
         self.file_name_gen = file_name_gen
         self.content_gen = content_gen
+        self.concurrent = concurrent
 
     def generate(self, n, lines_no=10):
         os.makedirs(self.dir_path)
         for i in self.file_name_gen(n):
-            self.__create_file(self.dir_path, i, self.content_gen, lines_no)
+            if self.concurrent:
+                self.__create_file_concurrent(self.dir_path, i, self.content_gen, lines_no)
+            else:
+                self.__create_file(self.dir_path, i, self.content_gen, lines_no)
+
+    @staticmethod
+    def __create_file_concurrent(dir_path, file_name, content_gen, lines_no=10):
+        t = threading.Thread(target=TempFileGenerator.__create_file,
+                             args=(dir_path, file_name, content_gen, lines_no))
+        t.start()
 
     @staticmethod
     def __create_file(dir_path, file_name, content_gen, lines_no=10):
@@ -49,11 +62,21 @@ if __name__ == "__main__":
     ''':type tempFolder: path'''
     tempFolder = path.join("/tmp/python", str(uuid.uuid4()))
 
-    files_no = 10
-    lines_no = 1000000
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--files-no', type=int, default="20")
+    parser.add_argument('--lines-no', type=int, default="10000")
+    parser.add_argument('--concurrent', type=bool, default="True")
+
+    args = parser.parse_args()
+    files_no = args.files_no
+    lines_no = args.lines_no
+    concurrent = args.concurrent
 
     ''':type generator: TempFileGenerator'''
-    generator = TempFileGenerator(tempFolder, uuid_generator, event_content_gen)
+    generator = TempFileGenerator(dir_path=tempFolder,
+                                  file_name_gen=uuid_generator,
+                                  content_gen=event_content_gen,
+                                  concurrent=concurrent)
     generator.generate(files_no, lines_no)
 
     assert path.exists(tempFolder), "Folder should be created by temp file generator!"
