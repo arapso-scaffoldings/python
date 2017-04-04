e000c90eb6e867425920100b68e087a6b50d0d12
Damian Ospara <damian@roq.ad>
2017-04-04 15:04:08 +0200

    Add luigi task parameter

--- luigi/run_luigi.py
+++ luigi/run_luigi.py
@@ -2,25 +2,27 @@
 import luigi
 
 class PrintNumbers(luigi.Task):
+    n = luigi.IntParameter()
 
     def requires(self):
         return []
 
     def output(self):
-        return luigi.LocalTarget("numbers_up_to_10.txt")
+        return luigi.LocalTarget("numbers_up_to_{}.txt".format(self.n))
 
     def run(self):
         with self.output().open('w') as f:
-            for i in range(1, 11):
+            for i in range(1, self.n + 1):
                 f.write("{}\n".format(i))
 
 class SquaredNumbers(luigi.Task):
+    n = luigi.IntParameter()
 
     def requires(self):
-        return [PrintNumbers()]
+        return [PrintNumbers(n = self.n)]
 
     def output(self):
-        return luigi.LocalTarget("squares.txt")
+        return luigi.LocalTarget("squares_{}.txt".format(self.n))
 
     def run(self):
         with self.input()[0].open() as fin, self.output().open('w') as fout:
